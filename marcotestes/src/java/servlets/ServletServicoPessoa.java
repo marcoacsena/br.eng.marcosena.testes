04995c27f0882878e25990e0db28ae0b794fe0b2
80119050 <80119050@MSC36171427.correiosnet.int>
2019-05-20 13:09:21 -0300

    Atividade 9: código do cliente da Servlet Estoque.

---
+++ marcotestes/src/java/servlets/ServletServicoPessoa.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.VO.Pessoa;
+
+/**
+ *
+ * @author 80119050
+ */
+public class ServletServicoPessoa extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           
+        URL url = new URL("http://localhost:8080/ExemploJSON-Serializacao/ServletServicoPessoa");
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        String inputLine;
+        String jsonString="";
+        while ((inputLine = in.readLine()) != null){
+            jsonString+=inputLine;
+        }
+        in.close();
+        
+        Gson gson = new GsonBuilder().create();
+        ArrayList<Pessoa> listaFromJSON = (ArrayList<Pessoa>)gson.fromJson(jsonString,new TypeToken<ArrayList<Pessoa>>(){}.getType());
+     
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletClientePessoa</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            for(Pessoa p: listaFromJSON){
+                out.println("<b>Nome:</b>"+p.getNome());
+                out.println("<br/>");
+                out.println("<b>Sobrenome:</b>"+p.getSobreNome());
+                out.println("<br/>");out.println("<br/>");out.println("<br/>");
+              
+            }
+            
+            out.println("</body>");
+            out.println("</html>");
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
